2f25db56cc92ffd5042073cc1b2b0f387c26d8f0
kadidia98 <diallokhadidiatoubouba@gmail.com>
2023-03-16 17:20:09 +0000

    réalisation du dashbord

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashbord', component: DashbordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
