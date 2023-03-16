fa5b9a07f05b747c9638ca56eb49006ab9ad078c
Josephine <sosodjibrildiop@gmail.com>
2023-03-16 06:16:05 +0000

    interface

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SystemeComponent } from './systeme/systeme.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'systeme',component:SystemeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/systeme/systeme.component.ts
+++ frontend/src/app/systeme/systeme.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-systeme',
   templateUrl: './systeme.component.html',
   styleUrls: ['./systeme.component.scss']
 })
-export class SystemeComponent {
-
+export class SystemeComponent implements OnInit {
+  bouton:any
+  ouvert='assets/ouvert.png'
+  fermer='assets/fermer.png'
+  ngOnInit(): void {
+      
+  }
 }
