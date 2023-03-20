e37f0a7d4ec1b9538ea1aac375dca286a93557f8
kadidia98 <diallokhadidiatoubouba@gmail.com>
2023-03-20 19:03:36 +0000

    modification router-outlet

--- frontend/src/app/dashbord/dashbord.component.ts
+++ frontend/src/app/dashbord/dashbord.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { donnee } from './../test2';
 
 @Component({
   selector: 'app-dashbord',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashbord.component.scss']
 })
 export class DashbordComponent {
+  temperature: any;
+  humidite: any;
+  lumiere: any;
+  humsol: any;
+  filter_entree!: any;
 
+  ngOnInit(){
+    this.filter_entree=donnee;
+     console.log(this.filter_entree)
+ 
+ 
+ 
+   }
 }
