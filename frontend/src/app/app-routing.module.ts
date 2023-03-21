fa3b4b19b9fc3b6a1ac1fa905f40743aa910c4a9
kadidia98 <diallokhadidiatoubouba@gmail.com>
2023-03-21 13:55:42 +0000

    rectification

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { HistoriqueComponent } from './historique/historique.component';
 
 const routes: Routes = [
   { path: 'dashbord', component: DashbordComponent },
-  { path:'systeme',component:SystemeComponent},
+  { path:'systeme',component: SystemeComponent},
   { path: 'login', component: FormulaireComponent},
   { path: 'map' , component: MapComponent},
   { path: 'historique', component:HistoriqueComponent},
